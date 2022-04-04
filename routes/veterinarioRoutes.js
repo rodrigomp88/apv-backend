a6dd91918ba24de68e401acfa13f8c4d35343eff
Rodrigo <rodrigomp88@gmail.com>
2022-04-03 22:12:02 -0300

    first commit

---
+++ routes/veterinarioRoutes.js
@@ -0,0 +1,17 @@
+import express from "express";
+const router = express.Router();
+import {
+  registrar,
+  perfil,
+  confirmar,
+  autenticar,
+} from "../controllers/veterinarioController.js";
+import checkAuth from "../middleware/authMiddleware.js";
+
+router.post("/", registrar);
+router.get("/confirmar/:token", confirmar);
+router.post("/login", autenticar);
+
+router.get("/perfil", checkAuth, perfil);
+
+export default router;
